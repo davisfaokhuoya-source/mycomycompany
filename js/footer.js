2dd1ae77a0fc3bdbdf62fff2fa6334e75f8bbce3
davisfaokhuoya-source <davisfaokhuoya@gmail.com>
2026-05-20 10:05:45 +0100

    new

--- js/footer.js
+++ js/footer.js
@@ -181,7 +181,3 @@ window.onload = function() {
   }
 };
 
-function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
-        o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
-        o.onload=function(){window.trackingFunctions.onLoad({appId:"6a03d9831e5705000df846a2"})},
-        document.head.appendChild(o)}initApollo();
